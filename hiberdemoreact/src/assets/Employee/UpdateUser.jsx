bf16e2b7683ad64c973eaa1a516ee2f6aa10ee28
pragya pardhi <127965506+Pragyapardhi11@users.noreply.github.com>
2024-12-13 00:30:06 +0530

    Update UpdateUser.jsx

--- hiberdemoreact/src/assets/Employee/UpdateUser.jsx
+++ hiberdemoreact/src/assets/Employee/UpdateUser.jsx
@@ -1,62 +1,58 @@
-import { Button } from 'bootstrap';
 import { useEffect, useState } from 'react';
-import { Form, useNavigate, useParams } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 import './UpdateUser.css';
 
 const UpdateUser = () => {
 
-  const {id} = useParams();
-  const navigate = useNavigate();
-
-  const [formData ,setformData] = useState({
-    name :"",
-    email : "",
-    department : ""
-    })
-
-
-    const handleInputChange = (event) =>{
-    const{name , value} = event.target;
-
-        setformData({
-            ...formData,
-            [name]:value,
-    })
-
-    }
-
-  useEffect(() =>{
-    const fetchEmployees = async () =>{
-      try{
+    const { id } = useParams();
+    const navigate = useNavigate();
+  
+    const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      department: ""
+    });
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+  
+    useEffect(() => {
+      const fetchEmployee = async () => {
+        try {
           const response = await fetch(`http://localhost:8080/api/employee/${id}`);
           const data = await response.json();
-          setformData(data);
-      }catch(error){
-          console.error("Error Fetching user :" ,error.message);
-      }
-    }
-    fetchEmployees();
-  },[id])
-
-const handleSubmit = async (e) =>{
-        e.preventDefault();
-
-        try{
-          const response = await fetch(`http://localhost:8080/api/employee/${id}` , {
-            method: 'PATCH',
-            headers:{
-              "Content-Type" : "application/json",
-            },
-            body: JSON.stringify(formData)
-         });
-            const data = await response.json();
-            console.log("User update :" ,data);
-
-            navigate(`/`)
-        }catch (error){
-            console.error("Error Updating User :", error.message);
+          setFormData(data);
+        } catch (error) {
+          console.error("Error Fetching user:", error.message);
         }
-}
+      };
+      fetchEmployee();
+    }, [id]);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      try {
+        const response = await fetch(`http://localhost:8080/api/employee/${id}`, {
+          method: 'PATCH',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData)
+        });
+        const data = await response.json();
+        console.log("User updated:", data);
+        navigate(`/`);
+      } catch (error) {
+        console.error("Error Updating User:", error.message);
+      }
+    };
 
 
   return (
@@ -64,36 +60,35 @@ const handleSubmit = async (e) =>{
     <div className='center-form'>
     <h1>Edit Employee</h1>
     <Form onSubmit={handleSubmit}>
-        <Form.Group controlId='formBasicName'>
-                <Form.Control
-                type ="text"
-                name = "name"
-                placeholder = "Enter name"
-                value = {formData.name}
-                onChange= {handleInputChange}
-                />
-        </Form.Group>
+    <Form.Group controlId='formBasicName'>
+    <Form.Control
+    type="text"
+    name="name"
+    placeholder="Enter name"
+    value={formData.name}
+    onChange={handleInputChange}
+    />
+</Form.Group>
 
-        <Form.Group controlId='formBasicName'>
-                <Form.Control
-                type ="email"
-                name = "email"
-                placeholder = "Enter email"
-                value = {formData.email}
-                onChange= {handleInputChange}
-                />
-        </Form.Group>
+<Form.Group controlId='formBasicEmail'>
+    <Form.Control
+    type="email"
+    name="email"
+    placeholder="Enter email"
+    value={formData.email}
+    onChange={handleInputChange}
+    />
+</Form.Group>
 
-        <Form.Group controlId='formBasicName'>
-                <Form.Control
-                type ="text"
-                name = "department"
-                placeholder = "Enter department"
-                value = {formData.department}
-                onChange= {handleInputChange}
-                />
-        </Form.Group>
-
+<Form.Group controlId='formBasicDepartment'>
+    <Form.Control
+    type="text"
+    name="department"
+    placeholder="Enter department"
+    value={formData.department}
+    onChange={handleInputChange}
+    />
+</Form.Group>
         <Button variant="Primary" type ="submit" className='w-100'>Edit Employee</Button>
     </Form>
     </div>
